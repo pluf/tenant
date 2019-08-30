766518536a217519a79e3d7be8eee321a58e17d2
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-08-30 23:32:44 +0430

    Fix bug in the process to create new tenant

--- src/Tenant/Service.php
+++ src/Tenant/Service.php
@@ -112,7 +112,8 @@ class Tenant_Service
         }
     }
 
-    public static function createNewTenant($data){
+    public static function createNewTenant($data)
+    {
         // Create a tenant
         $tenant = new Pluf_Tenant();
         // Set domain from subdomain if domain is not set in the request
@@ -123,37 +124,35 @@ class Tenant_Service
         $tenant->_a['cols']['parent_id']['editable'] = true;
         $currentTenant = Pluf_Tenant::current();
         $data['parent_id'] = $currentTenant->id;
-        
+
         $form = Pluf_Shortcuts_GetFormForModel($tenant, $data);
         $tenant = $form->save();
-        
+
         // Set path to initial data. It should be checked before switching to new tenant.
-        if(!$data['initial_default_data']){
-            $data['initial_default_data'] = Tenant_Service::setting('initial_default_data'); 
-        }
-        
+        $data['initial_default_data'] = Tenant_Service::setting('initial_default_data');
+
         // Init the Tenant
         $m = new Pluf_Migration(Pluf::f('installed_apps'));
         $m->init($tenant);
-        
+
         // TODO: hadi, 97-06-18: create account and credential base on given data by user in request
         // For example: login, password, list of modules to install and so on.
-        
+
         // TODO: update user api to get user by login directly
         $user = new User_Account();
         $user = $user->getUser('admin');
-        
+
         $credit = new User_Credential();
         $credit->setFromFormData(array(
             'account_id' => $user->id
         ));
         $credit->setPassword('admin');
         $credit->create();
-        
+
         // Set owner
         $role = User_Role::getFromString('tenant.owner');
         $user->setAssoc($role);
-        
+
         // install SPAcs
         $spas = Pluf::f('spas', array());
         if (sizeof($spas) > 0 && class_exists('Tenant_SpaService')) {
@@ -168,28 +167,33 @@ class Tenant_Service
                 throw new Pluf_Exception("Impossible to install spas from market.", 5000, $e, 500);
             }
         }
-        
+        Tenant_Service::provideContent($data);
         return $tenant;
     }
 
-    private static function provideContent($data){        
+    private static function provideContent($data)
+    {
         // Load initial default data
-        $path = $data['initial_default_data'];
-        $file = Pluf::f('temp_folder', '/tmp') . '/content-' . rand() . '.zip';
-        // Do request
-        $client = new GuzzleHttp\Client();
-        $response = $client->request('GET', $path, [
-            'sink' => $file
-        ]);
-        Backup_Service::loadData($file);
-        
-        $path = $data['initial_data'];
-        $file = Pluf::f('temp_folder', '/tmp') . '/content-' . rand() . '.zip';
-        // Do request
-        $client = new GuzzleHttp\Client();
-        $response = $client->request('GET', $path, [
-            'sink' => $file
-        ]);
-        Backup_Service::loadData($file);
+        if (array_key_exists('initial_default_data', $data)) {
+            $path = $data['initial_default_data'];
+            $file = Pluf::f('temp_folder', '/tmp') . '/content-' . rand() . '.zip';
+            // Do request
+            $client = new GuzzleHttp\Client();
+            $response = $client->request('GET', $path, [
+                'sink' => $file
+            ]);
+            Backup_Service::loadData($file);
+        }
+        // Load initial data
+        if (array_key_exists('initial_data', $data)) {
+            $path = $data['initial_data'];
+            $file = Pluf::f('temp_folder', '/tmp') . '/content-' . rand() . '.zip';
+            // Do request
+            $client = new GuzzleHttp\Client();
+            $response = $client->request('GET', $path, [
+                'sink' => $file
+            ]);
+            Backup_Service::loadData($file);
+        }
     }
 }
